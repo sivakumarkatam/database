998775a93ee6f0f1181953426808d7ad59972083
sivakumarkatam <siva.katam@blazeclan.com>
2017-09-11 15:26:02 +0530

    Update 007_add_column.sql

--- 007_add_column.sql
+++ 007_add_column.sql
@@ -1,8 +1,8 @@
 ALTER TABLE demo ADD newcolumn VARCHAR(30)
 ;
-INSERT INTO demo( newcolumn ) VALUES ( new ) WHERE demo_ID = 1
+update demo set newcolumn='new1' where demo_ID=1
 ;
-INSERT INTO demo( newcolumn ) VALUES ( new2 ) WHERE demo_ID = 2
+update demo set newcolumn='new2' where demo_ID=2
 ;
-INSERT INTO demo( newcolumn ) VALUES ( new3 ) WHERE demo_ID = 3
+update demo set newcolumn='new3' where demo_ID=3
 ;
