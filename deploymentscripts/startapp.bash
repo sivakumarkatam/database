a806fb8021fd414bd0869ad6540d3a659dbd3e9b
sivakumarkatam <siva.katam@blazeclan.com>
2017-09-11 14:35:31 +0530

    Update startapp.bash

--- deploymentscripts/startapp.bash
+++ deploymentscripts/startapp.bash
@@ -1,4 +1,12 @@
 #!/bin/bash
 cd /home/ubuntu
 #mvn install dbdeploy:update
-mvn install dbdeploy:update -Ddatabaseuser=dbdeploy -Ddatabasepassword=dbdeploy
+aws --region ap-southeast-1  ssm get-parameters --names "Dbuser" | awk 'match($0, /"Value":.*/) { print substr($0, RSTART, RLENGTH) }'| cut -d '"' -f4 | sudo tee user
+username=$(cat user)
+echo "${username}"
+
+aws --region ap-southeast-1  ssm get-parameters --names "Dbpassword" | awk 'match($0, /"Value":.*/) { print substr($0, RSTART, RLENGTH) }'| cut -d '"' -f4 | sudo tee pwd
+password=$(cat pwd)
+echo "${password}"
+
+mvn install dbdeploy:update -Ddatabaseuser=$username -Ddatabasepassword=$password
