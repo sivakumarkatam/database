b8d836436a7d2247b21a7d36d4ed86d13e29dfbe
sivakumarkatam <siva.katam@blazeclan.com>
2017-09-11 16:15:11 +0530

    Create 009_new-DB.sql

---
+++ 009_new-DB.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE testDB
+;
+use testDB
+;
+CREATE TABLE Persons (
+    PersonID INTEGER NOT NULL
+    ,LastName varchar(255)
+    ,FirstName varchar(255)
+    ,Address varchar(255)
+    ,City varchar(255) 
+)
+;
+
+INSERT INTO Persons (PersonID, LastName, FirstNmae, Address, City) VALUES (1, 'kumar', 'siva', 'Hinjiwadi', 'Pune')
+;
+INSERT INTO Persons (PersonID, LastName, FirstNmae, Address, City) VALUES (2, 'kumar', 'katam', 'ESI', 'Hyderabad')
+;
+INSERT INTO Persons (PersonID, LastName, FirstNmae, Address, City) VALUES (3, 'kumar', 'siva', 'Hinjiwadi', 'Mumbai')
+;
