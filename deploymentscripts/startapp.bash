954f52d3f0056cfde97e490cfa7a73e5413d4b2a
sivakumarkatam <siva.katam@blazeclan.com>
2017-09-11 16:59:32 +0530

    Update startapp.bash

--- deploymentscripts/startapp.bash
+++ deploymentscripts/startapp.bash
@@ -4,5 +4,5 @@ username=$(cat user)
 
 aws --region ap-southeast-1  ssm get-parameters --names "Dbpassword" | awk 'match($0, /"Value":.*/) { print substr($0, RSTART, RLENGTH) }'| cut -d '"' -f4 | sudo tee pwd
 password=$(cat pwd)
-
+cd /tmp/
 mvn install dbdeploy:update -Ddatabaseuser=$username -Ddatabasepassword=$password
